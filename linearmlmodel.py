a89e8a127960b3d29fbcc21c7181c11b5eee2de1
rahulnobcoder <111956749+rahulnobcoder@users.noreply.github.com>
2022-08-27 23:15:02 +0530

    Update linearmlmodel.py

--- linearmlmodel.py
+++ linearmlmodel.py
@@ -7,7 +7,6 @@ plt.rcParams['figure.figsize']=(20.0,10.0)
 data = p.read_csv('ds_salaries.csv')
 data.head()
 #default take x and y in caps to represent columns etc and remove this comment before copying this code to paste anywhere
-an 
 x=data['salary'].values
 y=data['number'].values
 mean_x=np.mean(x)
@@ -22,7 +21,8 @@ for i in range(m):
     denom = denom + (x[i]-mean_x)**2
 b1=numer/denom
 b0=mean_y - (b1*mean_x)
-print(b1,b0)
+print("b1 is ",b1)
+print("b0 is ",b0)
 max_x = np.max(x) 
 min_x = np.min(x)
 a=np.linspace(min_x,min_x,len(x))
@@ -33,3 +33,13 @@ plt.xlabel('salary')
 plt.ylabel('number')
 plt.legend()
 plt.show
+ss_t= 0
+ss_r = 0
+for i in range(m):
+    y_pred=b0+b1+a[i]
+    ss_t += (b[i]-mean_y)**2
+    ss_r += (b[i]-y_pred)**2
+r2=1-(ss_r/ss_t)
+print("r2 is ")
+print(r2)
+  #r2 is r squared  
